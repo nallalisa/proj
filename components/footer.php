ba11072ffe4bd02ad6e8c62ed26b9d84da9e1518
unknown <Nallalisa>
2022-12-12 04:06:42 +0800

    [inital] commit

---
+++ components/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href=""><i class="fas fa-phone"></i> +639665868848</a>
+         <a href=""><i class="fas fa-envelope"></i> reymalyn.sc@gmail.com</a>
+         <a href=""><i class="fas fa-map-marker-alt"></i> Caloocan City, Philippines</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/mariaswardrobeph"><i class="fab fa-facebook-f"></i>facebook</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ITsy Inc.</span> | all rights reserved!</div>
+
+</footer>
